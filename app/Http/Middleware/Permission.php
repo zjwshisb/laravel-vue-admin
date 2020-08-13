cec0a6ba6c3c2b4de6982580221724b44e6a2162
zjwshisb <361340272@qq.com>
2020-08-13 11:15:53 +0800

    update

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Http\Middleware;
+
 use Closure;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Support\Facades\Route;
@@ -8,7 +10,8 @@ use Illuminate\Support\Facades\Route;
  * Class Permission
  * @package App\Http\Middleware
  */
-class Permission {
+class Permission
+{
     /**
      * @param $request
      * @param Closure $next
@@ -18,11 +21,12 @@ class Permission {
     public function handle($request, Closure $next)
     {
         $user = \Auth::user();
-        if($user && $user->is_super != 1) {
-            $action = Route::current()->getAction()['controller'] ?? '';
-            if (!$action || $user->can($action)) {
-                return $next($request);
-            }
+        if ($user->is_super) {
+            return $next($request);
+        }
+        $action = Route::current()->getAction()['controller'] ?? '';
+        if (!$action || $user->can($action)) {
+            return $next($request);
         }
         throw new AuthorizationException();
     }
