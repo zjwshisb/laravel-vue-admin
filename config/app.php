719a522f93e6e1106e2dbdb3a457c79cdc447d8b
zjwshisb <361340272@qq.com>
2020-08-13 10:50:09 +0800

    update

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Shanghai',
 
     /*
     |--------------------------------------------------------------------------
@@ -167,7 +167,6 @@ return [
          */
         Overtrue\LaravelLang\TranslationServiceProvider::class,
         EloquentFilter\ServiceProvider::class,
-        Overtrue\LaravelPinyin\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -217,7 +216,7 @@ return [
         'Password' => Illuminate\Support\Facades\Password::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
         'Redirect' => Illuminate\Support\Facades\Redirect::class,
-        'Redis' => Illuminate\Support\Facades\Redis::class,
+        'PRedis' => Illuminate\Support\Facades\Redis::class,
         'Request' => Illuminate\Support\Facades\Request::class,
         'Response' => Illuminate\Support\Facades\Response::class,
         'Route' => Illuminate\Support\Facades\Route::class,
